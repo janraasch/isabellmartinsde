01d149257c480fad4be30960619bbb5a960d48a3
Jan Raasch <jan@janraasch.com>
2020-08-27 17:53:25 +0200

    chore: overwrite formhandler

---
+++ hunde/assets/js/formhandler.js
@@ -0,0 +1,28 @@
+window.addEventListener("DOMContentLoaded", function() {
+    var form = document.getElementById("kontakt");
+    var $form = $(form);
+    var button = document.getElementById("contact-form-button");
+    var status = document.getElementById("contact-form-status");
+
+    function success() {
+      status.innerHTML = status.dataset.success
+    }
+
+    function error() {
+      button.style = "";
+      status.innerHTML = status.dataset.error
+    }
+
+    if (!form) {
+      // I guess we are not on the contact-page... :-)
+      return;
+    }
+
+    // handle the form submission event
+    // see https://docs.netlify.com/forms/setup/#submit-forms-via-ajax
+    form.addEventListener("submit", function(event) {
+      button.style = "display: none";
+      event.preventDefault();
+      $.post($form.attr("action"), $form.serialize()).done(success).fail(error);
+    });
+  });
